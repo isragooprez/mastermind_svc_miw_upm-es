044f828de8e363e958396460d919be368f1fdfd7
isragooprez <isragoo.prez@gmail.com>
2017-12-04 20:19:53 -0500

    initial commit

---
+++ src/main/java/mastermind/MasterMindConsole.java
@@ -0,0 +1,13 @@
+package mastermind;
+
+public class MasterMindConsole {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //System.out.println("hola mundo");
+        
+     
+    }
+  
+
+}
