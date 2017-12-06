cf21bc2bea753cbfff88793f58e0beb4d94f70d2
isragooprez <isragoo.prez@gmail.com>
2017-12-05 20:35:15 -0500

    Eliminación de Comentarios innecesarios

--- src/main/java/mastermind/MasterMindConsole.java
+++ src/main/java/mastermind/MasterMindConsole.java
@@ -4,11 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MasterMindConsole {
-    /**
-     * Metodo principal
-     * 
-     * @param args
-     */
+   
     public static void main(String[] args) {
         String clave2;
         Scanner tec = new Scanner(System.in);
@@ -38,46 +34,28 @@ public class MasterMindConsole {
         }
         System.out.println("Puntuación= " + i);
     }
-    /**
-     * Metodo que calcula los muertos acertado en colo y posision
-     * 
-     * @param clave1 clave1
-     * @param clave2 clave1
-     * @return el numero de muertos
-     */
+    
     public static int muertos(String clave1, String clave2) {
         int comp = 0;
-        // int clave1l = clave1.length();
-        // int clave2l = clave2.length();
+    
         int a = 0;
-        // recorre el array de muertos
-        for (comp = 0; comp < 5; comp++) { // for
+        for (comp = 0; comp < 5; comp++) { 
             char c2 = clave2.charAt(comp);
             char c1 = clave1.charAt(comp);
-
             if (c1 == c2) {
                 a++;
             }
         }
-        // System.out.println( clave1l + " " + clave2l);
         return a;
     }
-    /**
-     * Metodo que calcula los heridos
-     * 
-     * @param clave1 clave 1
-     * @param clave2 clave 2
-     * @return el numero de heridos
-     */
+    
     public static int heridos(String clave1, String clave2) {
         int n = 0;
         int a, b;
         int h = 0;
-        // recore el for
         for (int comp = 0; comp < 5; comp++) {
             char c2 = clave2.charAt(comp);
             char c1 = clave1.charAt(comp);
-
             int clave1l = clave1.length();
             int clave2l = clave2.length();
             n = clave1.indexOf(c2);
